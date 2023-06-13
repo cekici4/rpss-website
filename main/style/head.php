6dd0df6e7b6b4ee2a43f408724978bc8d134cb34
Cagri <100359589+cekici4@users.noreply.github.com>
2023-06-13 13:18:51 +0200

    api and default document work now
    :)

--- main/style/head.php
+++ main/style/head.php
@@ -11,14 +11,14 @@
 
 <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sidebars/">
 <!-- Define Base root of project -->
-<base href="http://localhost:8012/rpss-website/main/" target="_self">
+<!--<base href="rpss-website/main/" target="_self"> -->
 <!-- CSS stylesheets -->
 <!-- <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet"> -->
-<link href="http://localhost:8012/rpss-website/main/style/main.css" rel="stylesheet">
+<link href="./style/main.css" rel="stylesheet">
 
 <!-- JavaScript -->
-<!-- <script src="http://localhost:8012/rpss-website/main/code/main.js" defer></script> -->
-<script src="http://localhost:8012/rpss-website/main/code/optimize.js" defer></script>
+<script type="text/JavaScript" src="./code/main.js" defer></script>
+<script type="text/JavaScript" src="./code/optimize.js" defer></script>
 
 <!-- Third-party libraries -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
